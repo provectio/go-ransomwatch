d69a3ad7a5e6805de1aa4bebc3a5919dffa2396e
f41k4l <Provectio>
2023-09-27 19:37:13 +0200

    corrrecting matchs list

--- match-orgs-name.go
+++ match-orgs-name.go
@@ -6,9 +6,9 @@ import (
 
 // Return a map of orgs name matched with their post and group.
 // If exact is true, the match is done with the exact name of the org.
-func MatchOrgsName(exact bool, orgs ...string) (matchs map[string]Match, err error) {
+func MatchOrgsName(exact bool, organisations ...string) (matchs map[string][]Match, err error) {
 
-	matchs = make(map[string]Match)
+	matchs = make(map[string][]Match)
 
 	posts, err := GetPosts()
 	if err != nil {
@@ -25,19 +25,19 @@ func MatchOrgsName(exact bool, orgs ...string) (matchs map[string]Match, err err
 		groupsByName[group.Name] = group
 	}
 
-	for _, org := range orgs {
+	for _, org := range organisations {
 		for _, post := range posts {
 			if exact && post.Title == org {
-				matchs[org] = Match{
+				matchs[org] = append(matchs[org], Match{
 					Post:  post,
 					Group: groupsByName[post.GroupName],
-				}
+				})
 			} else if lowerOrg, lowerTitle := strings.ToLower(org), strings.ToLower(post.Title); !exact &&
 				strings.Contains(lowerTitle, lowerOrg) {
-				matchs[org] = Match{
+				matchs[org] = append(matchs[org], Match{
 					Post:  post,
 					Group: groupsByName[post.GroupName],
-				}
+				})
 			}
 		}
 	}
